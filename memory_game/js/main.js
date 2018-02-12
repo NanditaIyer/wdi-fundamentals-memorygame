f6d491d36812e4114d07794834c781cdcfd5a9f0
NanditaIyer <35190753+NanditaIyer@users.noreply.github.com>
2018-02-12 11:24:56 +0530

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -41,24 +41,31 @@ alert("Sorry, try again.");
 
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+cardId = this.getAttribute('data-id');
+console.log(cardId);
 
-console.log("User flipped" +" "+ cards[cardId].rank)
+cardsInPlay.push(cards[cardId].card);
 
-cardsInPlay.push(cards[cardId].rank)
+this.setAttribute('src', cards[cardId].cardImage);
 
 if (cardsInPlay.length === 2) {
-
-}
-
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-
-
 checkForMatch();
-
-
+cardsInPlay = [];
+}
 };
 
-flipCard(0);
-flipCard(2);
+
+
+var createBoard = function() {
+for (var i = 0; i < cards.length; i++) {
+var cardElement = document.createElement('img');
+cardElement.setAttribute('src', 'images/back.png');
+cardElement.setAttribute('data-id', i);
+cardElement.addEventListener('click', flipCard);
+document.getElementById('game-board').appendChild(cardElement);
+}
+}
+createBoard();
+
+
